c303027a03cf492abdfe7f39a4da241abdf61117
jhonaathan95 <jhonathan.alvarez@ucp.edu.co>
2017-11-15 20:49:40 -0500

    jhonatahn
    parcial

--- application/controllers/Primero.php
+++ application/controllers/Primero.php
@@ -34,8 +34,8 @@ class Primero extends CI_Controller {
 			$dos= "2";
 			
 			if($this->Usuarios->existe($username, $password, $uno)) {
-				echo '<script language="javascript">alert("alguno de los campos esta incorrecto verificar");</script>';
-				$this->load->view('menu');
+				echo '<script language="javascript">alert("uno de las claves estan mal");</script>';
+				$this->load->view('primera');
 				
 			}else{
 			
